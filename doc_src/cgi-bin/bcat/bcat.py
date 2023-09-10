46769c730e2f3f613fedea8e0d8c4e33b9d2d288
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-09-10 22:02:59 +0300

    FIXED action plan in bcat.py (ref getbooks catalog)

--- doc_src/cgi-bin/bcat/bcat.py
+++ doc_src/cgi-bin/bcat/bcat.py
@@ -7,10 +7,21 @@
 #-----------------------------------------------------------
 
 import os
-import pysondb
+
+# import urllib3 #TODO install it or use urllib which is built in
+
+# import cgi #NOT this or next one? or both?
+import cgitb 
+
+# import pysondb #NOTE ck if can be used with a dbs as string...
+#NOTE the JSON file is obtained by mk a request to `data/books_catalog.json` which return a response obtainable as resp.get_json() - see requests or urllib(3) module ref how to get json from response (or cosana)
+
+
+
 
 my_name = __name__
 my_crt_dir = os.getcwd()
+cgitb.enable() # this activate displaying errs on HTML page and log them...
 
 
 # HTTP header section
@@ -25,7 +36,8 @@ print()
 
 - remake `path(my_crt_dir, "data/books_catalog.json)` to be opened with `pysondb` (meaning "data" key) ATTN actual table loading in bcat.md will not work anymore so mk a copy for temporary use
 
-- get all recs of (key `"data"` but psyondb already did...)
+- TRY THIS: get all recs of (key `"data"` but psyondb already did...)
+- OR DO: request to `data/books_catalog.json`
 
 - get file `/bcat/bcat.html` and  Jinja render it
 
