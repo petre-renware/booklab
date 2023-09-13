033b06ce91c5134027f9ab96f7962fc41d8c0949
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-09-13 18:59:32 +0300

    Update bkcmd_new_book.py

--- doc_src/book_tmpl/bkcmd_new_book.py
+++ doc_src/book_tmpl/bkcmd_new_book.py
@@ -1,10 +1,13 @@
 ################
+#
+# 0. this code is subject of CGI NEWB
+#     - remark that this file is agnostic of book template and should stay in doc_src/
+# --------------
 # 1. create a new directory for the new book - this will act as a sandbox for all new book
 # 2. unzip the template files (zip with `bk_tml/` directory
 # 3. change / update the config files (yml, bkcmd...sh, ...}
+# 4. depending on how designed, wr some metadata ref never open of new book
 #
-# 4. #NOTE subject of not yet made design (piu@230830)
-#     - depending on how designed, wr some metadata for a next open of this book
 ################
 
 # #-FIXME use mkdocs build with params like explained here `https://www.mkdocs.org/user-guide/cli/#mkdocs-get-deps`
