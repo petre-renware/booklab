9e7ff8dd2af4f7a7695d3ad93c82c3ee0838443a
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-09-13 19:01:31 +0300

    Update bkcmd_new_book.py

--- doc_src/book_tmpl/bkcmd_new_book.py
+++ doc_src/book_tmpl/bkcmd_new_book.py
@@ -1,7 +1,7 @@
 ################
 #
 # 0. this code is subject of CGI NEWB
-#     - remark that this file is agnostic of book template and should stay in doc_src/
+#     - remark that this file is agnostic of book template and stay in doc_src/
 # --------------
 # 1. create a new directory for the new book - this will act as a sandbox for all new book
 # 2. unzip the template files (zip with `bk_tml/` directory
