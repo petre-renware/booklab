4b277661defb74146d0e34d14898994f6ade66e8
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-09-18 01:33:27 +0300

    Update __model.py

--- doc_src/cgi-bin/__model.py
+++ doc_src/cgi-bin/__model.py
@@ -1,1 +1,22 @@
+#!pyenv/bin/python3
+
+#=============================================================
+# Script to #TODO objective here...
+#-----------------------------------------
+# Author: Petre Iordanescu, (c) RENware Softwre Sytems
+#=============================================================
+
+
+
+import os
+import cgi
+import cgitb
+import pysondb
+import jinja2
+
+
+my_name = __name__
+my_crt_dir = os.getcwd()
+cgitb.enable() # this activate displaying errs on HTML page and log them...
+
 
