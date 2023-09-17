efea8d781a716bc9f6b86be2f6e99353e646d9d5
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-09-18 02:10:30 +0300

    Update __model.py

--- doc_src/cgi-bin/__model.py
+++ doc_src/cgi-bin/__model.py
@@ -18,44 +18,49 @@ import jinja2
 my_name = __name__
 my_crt_dir = os.getcwd()
 cgitb.enable() # this activate displaying errs on HTML page and log them...
-"""
-Content-type:
-
-A MIME string defining the format of the file being returned. Example is Content-type:text/html
-
-2	
-Expires: Date
-
-The date the information becomes invalid. It is used by the browser to decide when a page needs to be refreshed. A valid date string is in the format 01 Jan 1998 12:00:00 GMT.
-
-3	
-Location: URL
-
-The URL that is returned instead of the URL requested. You can use this field to redirect a request to any file.
-
-4	
-Last-modified: Date
-
-The date of last modification of the resource.
-
-5	
-Content-length: N
-
-The length, in bytes, of the data being returned. The browser uses this value to report the estimated download time for a file.
-
-6	
-Set-Cookie: String
-
-Set the cookie passed through the string
-"""
 
 
-
-
-#NOTE ------------
-# HTTP header section for HTML content
+#-------------
+# #NOTE: HTTP header section for HTML content
 print("Content-Type: text/html\n")
 print()
+
+""" #NOTE other HTTP header items
+------
+Content-type: String
+
+* A MIME string defining the format of the file being returned. Example is Content-type:text/html
+
+------
+Expires: Date
+
+* The date the information becomes invalid. It is used by the browser to decide when a page needs to be refreshed. A valid date string is in the format 01 Jan 1998 12:00:00 GMT.
+
+------
+Location: URL
+
+* The URL that is returned instead of the URL requested. You can use this field to redirect a request to any file.
+
+------
+Last-modified: Date
+
+* The date of last modification of the resource.
+
+------
+Content-length: N
+
+* The length, in bytes, of the data being returned. The browser uses this value to report the estimated download time for a file.
+
+------
+Set-Cookie: String
+
+* Set the cookie passed through the string
+"""
+
+
+
+
+
 
 
 #NOTE ------------
