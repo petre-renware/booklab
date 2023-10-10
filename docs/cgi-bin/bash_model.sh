16aa6dd5c8b3699f91fc1f7a1f23b4d2a2eb5f4b
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-10-10 06:25:23 +0300

    published 231010 h06:30

--- docs/cgi-bin/bash_model.sh
+++ docs/cgi-bin/bash_model.sh
@@ -13,5 +13,12 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" #NOTE ... comment this code if is not required to change current location to those weher script is located
 
 
+
+# kill last launched process:  `kill "$!"`
+# show last process PID: `echo "$!"`
+# kill "$!"
+
+
+
 #NOTE ... continue from here
 
