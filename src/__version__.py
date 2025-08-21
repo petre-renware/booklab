8d9d4825282fb550591ce93a41bff398ee1b16e9
Petre Iordanescu <petre.iordanescu@gmail.com>
2025-08-21 18:04:49 +0300

    Update __version__.py

--- src/__version__.py
+++ src/__version__.py
@@ -1,4 +1,10 @@
 """
-...wip ... package version
+Booklab package version
+
+* Author: Petre Iordanescu (petre.iordanescu@gmail.com)
+* Crested at: 20-Aug-2025
+* Updated at: 21-Aug-2025
 """
+__version__ = "0.3.dev2"
+
 
