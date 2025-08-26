eb5e47241626c60398aee30bc58b9dd3421f88d8
Petre Iordanescu <petre.iordanescu@gmail.com>
2025-08-26 22:44:21 +0300

    Update app_init.py

--- src/booklab/booklabd/app_init.py
+++ src/booklab/booklabd/app_init.py
@@ -2,7 +2,6 @@
 This module is designed to initialize the main web application object `api_app`.
 
 Author: Petre Iordanescu (petre.iordanescu@gmail.com)
-Created: 25-aug-2025
 """
 
 from flask import Flask
