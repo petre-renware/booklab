235709fcbfd5dd174ab66ce2189ea5ebcdc1ec85
Application admin <app@booklab.lxd>
2025-08-31 07:44:06 +0000

    250831-c cteated object db_book

--- src/booklab/booklabd/__init__.py
+++ src/booklab/booklabd/__init__.py
@@ -8,8 +8,10 @@ Architecture; HTTP REST
 Author: Petre Iordanescu (petre.iordanescu@gmail.com)
 Created; 21-Aug-2025
 """
-
+# general imports
 import os
+import pysondb
+# booklab package imports
 from booklab.booklabd.app_init import init_app
 from booklab import PACKAGE_ROOT
 from booklab import CONF_ROOT
@@ -22,12 +24,12 @@ api_app = init_app(
     __name__,
     static_site_dir = template_location
 )
-# 4dbg... print(f"*** booklabd.__init__ *** Created {api_app=} with {template_location=}")
+# 4dbg... print(f"*** booklabd.__init__ Created {api_app=} with {template_location=}")
 
 # ...wip.opiss.230831-c create database object by opening JSON files 
-# dbs_file = os.path.join(my_crt_dir, "data/books_catalog.json")
-db_books = ...
-bcat_dbs = pysondb.db.getDb(dbs_file)
+bks_catalog_file = os.path.join(DATA_ROOT, "books_catalog.json")
+db_books = pysondb.db.getDb(bks_catalog_file)
+print(f"*** booklabd__init__ created object {db_books=}")
 db_system = ...
 #  the other db is app_info.json
 # 4dbg... print(f"*** booklabd.__init__ imported {PACKAGE_ROOT=} {DATA_ROOT=} {CONF_ROOT=}")
