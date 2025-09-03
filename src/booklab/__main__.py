1699d1b35d29c5447be3fab683e695014e7df55b
Application admin <app@booklab.lxd>
2025-09-03 08:18:07 +0000

    small improvents in package inside addressing modules

--- src/booklab/__main__.py
+++ src/booklab/__main__.py
@@ -4,5 +4,11 @@ modules to be launched at package call with `-m` option
 
 """
 
+from . import __version__ as sysver
+
 # this is just a tesr
 print("I'm in main module executing what is here...")
+print(f"System version is: {sysver=}")
+
+
+
