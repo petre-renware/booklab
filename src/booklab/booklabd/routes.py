3331ffd963b1484f60398ec371c5f8e0d00f313a
Petre Iordanescu <petre.iordanescu@gmail.com>
2025-09-07 01:01:45 +0300

    Update routes.py

--- src/booklab/booklabd/routes.py
+++ src/booklab/booklabd/routes.py
@@ -37,7 +37,7 @@ def api_bcat():
     # #TODO write rendered_str to file
     # .../docs/bcat/bcat.html
     file_to_write = os.path.join(pjroot_location, "docs/bcat/bcat.html")
-    with open(file_to_write, "a") as file:
+    with open(file_to_write, "w") as file:
         file.write(rendered_str)
     return redirect("/docs/bcat/bcat.html")  # ? use /booklab/ ???
 
