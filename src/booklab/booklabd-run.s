37a73fa52df6062cc2715a1cc5384c9c0f1eb4bb
Application admin <app@booklab.lxd>
2025-09-08 13:53:08 +0000

    [0.4b9] released

--- src/booklab/booklabd-run.s
+++ src/booklab/booklabd-run.s
@@ -18,7 +18,7 @@
 
 case $1 in
 d) gunicorn -c gunicorn.conf.py -p gunicorn.PID -D;;
-k) kill `cat gunicorn.PID`;;
+k) kill `cat gunicorn.PID;;
 s) ps -A | grep gunicorn;;
 *) gunicorn -c gunicorn.conf.py;;
 esac
