6f5f982a852694ddf9a5d0f52a797316ce735931
Petre Iordanescu <petre.iordanescu@gmail.com>
2025-09-16 14:33:51 +0300

    Update __init__.py

--- src/booklab/data/__init__.py
+++ src/booklab/data/__init__.py
@@ -1,7 +1,7 @@
 """
 **data/** component contains the application database as JSON files:
 
-* a books database containing all created books and detailed information about each one
-* a system database containing information about installed application
+* a books database (``) containing all created books and detailed information about each one
+* a system database (``) containing information about installed application
 """
 
