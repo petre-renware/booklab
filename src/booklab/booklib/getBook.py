98802a38243111e821cfb8e503430adb7b1d11aa
Petre Iordanescu <petre.iordanescu@gmail.com>
2025-09-18 04:49:15 +0300

    Update getBook.py

--- src/booklab/booklib/getBook.py
+++ src/booklab/booklib/getBook.py
@@ -1,12 +1,19 @@
-
+import pysondb
 
 def getBook(
     book_code: str
 ) -> list[dict] | None:
     """
+    Check for a given book code:
+    
+    * test if book code is not None
+    * test if book code exists in database and is exactly 1 record
+    * if both conditions return that record as List[Dict]
+    * if not any condition return None
 
+    Return:
 
-
+    list of dictionary wiyh found record
     """
     pass  # code from here ...
 
